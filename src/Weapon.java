093489f40d43ce71ec3556d7545577bda3c81e40
Craig D'Silva <craig.dsilva10@gmail.com>
2026-04-20 19:04:16 +0100

    Weapon class

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+public class Weapon extends Item {
+    private int damage;
+    private String type;
 
-public class Weapon {
+    public Weapon(String name, int quantity, int damage, String type) {
+        super(name, quantity);
+        this.damage = damage;
+        this.type = type;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
